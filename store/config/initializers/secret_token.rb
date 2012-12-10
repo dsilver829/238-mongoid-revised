f89fb778e76d2efd4101954f0f60a1c86d7447b7
David Silver <dsilver@candidatemetrics.com>
2012-12-10 08:53:51 -0800

    create store app

---
+++ store/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '05525dbb1275d2ed016c99466184bb0e4204a5d6914cec5d2509e4ad7134de6eb149510fd51e8cc9dc45fe2de08d429559a96b3af1c1db451a03477ee2785390'
