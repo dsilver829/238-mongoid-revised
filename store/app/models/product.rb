e5d7d32982a05dc9ce5725d20466f8402f421336
David Silver <dsilver@candidatemetrics.com>
2012-12-10 08:57:01 -0800

    add validation

--- store/app/models/product.rb
+++ store/app/models/product.rb
@@ -3,4 +3,7 @@ class Product
   field :name, type: String
   field :price, type: BigDecimal
   field :released_on, type: Date
+
+  attr_accessible :name, :price, :released_on
+  validates_presence_of :name
 end
